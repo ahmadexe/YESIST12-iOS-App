7b312eacf9e420c8a5b93d071c061d7043892763
ahmadexe <muahmad710@gmail.com>
2023-03-12 16:04:54 +0500

    app structured

--- lib/screens/timeline/timeline_screen.dart
+++ lib/screens/timeline/timeline_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:yesist_ios_app/configs/configs.dart';
+import 'package:yesist_ios_app/static/constants.dart';
+
+part 'widgets/_timeline_card.dart';
 
 class TimelineScreen extends StatelessWidget {
   const TimelineScreen({super.key});
@@ -6,7 +10,29 @@ class TimelineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        toolbarHeight: AppConstants.toolbarHeight2,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Timeline", style: AppText.h2!),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("March 2023", style: AppText.b2!),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("03:11 PM", style: AppText.b2!),
+                    Text("PST", style: AppText.b2!)
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+        centerTitle: false,
+      ),
     );
   }
 }
--- lib/static/constants.dart
+++ lib/static/constants.dart
@@ -2,4 +2,5 @@ import 'package:yesist_ios_app/configs/app_dimensions.dart';
 
 class AppConstants {
   static final toolbarHeight = AppDimensions.normalize(55);
+  static final toolbarHeight2 = AppDimensions.normalize(40);
 }
