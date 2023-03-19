f03c308fcd8847550c1644d54083675b9f8e2c33
ahmadexe <muahmad710@gmail.com>
2023-03-19 20:33:22 +0500

    header mroe info screen added

--- lib/configs/static_colors.dart
+++ lib/configs/static_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class StaticColors {
   static const primaryColor = Color.fromRGBO(10, 65, 92, 1);
   static const secondaryColor = Color.fromRGBO(90, 228, 168, 1);
+  static const greyBG = Color.fromRGBO(196, 196, 196, 0.17);
 }
--- lib/screens/more_info/more_info_screen.dart
+++ lib/screens/more_info/more_info_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yesist_ios_app/configs/app_typography.dart';
+import 'package:yesist_ios_app/configs/static_colors.dart';
+import 'package:yesist_ios_app/static/constants.dart';
 
 class MoreInfoScreen extends StatelessWidget {
   const MoreInfoScreen({super.key});
@@ -7,7 +10,21 @@ class MoreInfoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("More Info"),
+        toolbarHeight: AppConstants.toolbarHeight2,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/logos/logo.png'),
+            Text(
+              "A program under ASIST - Augmented Social Innovations through Science and Technology",
+              style: AppText.l2!.copyWith(color: Colors.black),
+              maxLines: 2,
+            ),
+          ],
+        ),
+        elevation: 0,
+        backgroundColor: StaticColors.greyBG,
+        centerTitle: false,
       ),
     );
   }
